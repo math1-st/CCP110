fb0b0d8d4724bb4ddce53ba8d5cc8cbd072db148
Matheus <227284362+math1-st@users.noreply.github.com>
2025-08-26 21:42:58 -0300

    Facilitação da leitura do resultado final.

--- aula3/exercicio3.py
+++ aula3/exercicio3.py
@@ -1,10 +1,12 @@
 print("Cálculo da soma de dias, horas, minutos e segundos em apenas segundos.")
 
 dias = int(input("Digite a quantidade de dias: "))
-horas = int(input("Digite o tempo em horas: "))
-minutos = int(input("Digite o tempo em minutos: "))
-segundos = int(input("Digite o tempo em segundos: "))
+horas = int(input("Digite a quantidade de horas: "))
+minutos = int(input("Digite a quantidade de minutos: "))
+segundos = int(input("Digite a quantidade de segundos: "))
 
 calculo = (dias * 24 * 3600) + (horas * 3600) + (minutos * 60) + segundos
 
-print(f"Resultado: {calculo} segundos")
+print(f"Resultado: {calculo:,d} segundos")
+
+# Adicionar :,d ao final de calculo para facilitar a leitura do número.
