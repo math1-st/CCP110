ba2638ce488d26c74ca92fe80a225ad73e4c5fc8
Matheus <227284362+math1-st@users.noreply.github.com>
2025-08-28 22:38:40 -0300

    Adicionado exercícios 1-7; Falta fazer 8º.

---
+++ aula4/exercicio8.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+altura_cilindro = 
+
+if qnt_lata == 1:
+    valor_lata_unid == 50
+elif qnt_lata == 2:
+    valor_lata_und == 48
+elif qnt_lata == 3:
+    valor_lata_und == 46
+else:
+    valor_lata_und == 45
